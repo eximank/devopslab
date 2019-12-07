11c4f239eb3a8a08d1585e2686ddec70d7fa0a2e
eximbank <dxh1224@gmail.com>
2019-12-07 13:56:21 +0800

    update setenv.sh

--- src/setenv.sh
+++ src/setenv.sh
@@ -1,7 +1,7 @@
 export GITACCOUNT=eximbank
 
 export MYCLUSTER=tektonknativebeijing09
-export KUBECONFIG=
+export KUBECONFIG=/tmp/ic/AB/.bluemix/plugins/container-service/clusters/tektonknativebeijing09/kube-config-tok04-tektonknativebeijing09.yml
 
 export NAMESPACE=eximbankcluster
 export EMAIL=dxh1224@gmail.com
